05b531a511baa4d3c424239b76c7e8f879913269
Stefan Balsiger <ste.balsiger@outlook.com>
2017-02-06 13:48:16 +0100

    Timestamp added to windsensor_data.txt

--- grab_serial_wind_sensor_data.py
+++ grab_serial_wind_sensor_data.py
@@ -1,6 +1,12 @@
 import serial
 import time
 
+human_time = ""
+
+#Open File
+windsensor_data = open("windsensor_data.txt","a+")
+
+
 arduino = serial.Serial("/dev/ttyACM0")
 arduino.baudrate=57600
 
@@ -8,9 +14,16 @@ arduino.baudrate=57600
 while True:
 	try:
 	    data = arduino.readline()
+	    human_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+
+	    print(human_time)
 	    print(data)
+
+	    windsensor_data.write(human_time+" ")
+	    windsensor_data.write(data);
 
 	except IOError:
 	    print("Error")
 
 arduino.close() 
+windsensor_data.close()
