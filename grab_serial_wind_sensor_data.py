8123a1d4c50eccf6332bb1325caed193ae6248ba
Stefan Balsiger <ste.balsiger@outlook.com>
2017-02-07 15:44:35 +0100

    tmp thermistor in volts finished incl. smoothing; LOG and DATA File added

--- grab_serial_wind_sensor_data.py
+++ grab_serial_wind_sensor_data.py
@@ -23,8 +23,8 @@ windSpeed_MPH_buffer = []
 data_corr = 10 #Data Correction Parameter in Percentage. 10% means that a data set n which differs more than 10% from a dataset n-1 will be sorted out.
 
 #Data output
-windsensor_data = open("windsensor_data.txt","a+")
-
+sensor_node_data = open("sensor_node_data.txt","a+")
+sensor_node_log = open("sensor_node.log", "a+")
 #Serial connection to arduino
 arduino = serial.Serial("/dev/ttyACM0")
 arduino.baudrate=57600
@@ -40,11 +40,16 @@ while True:
 	    rv_volts_buffer = []
  	    zeroWind_volts_buffer = []
  	    windSpeed_MPH_buffer = []
+	    time_buffer = []
 	    data_buffer_count = 0
-	    print("######################################################")	
+	    print("######################################################")
+	    sensor_node_log.write("######################################################\n")	
 	    while (data_buffer_count <2):
 		
 		data = arduino.readline()
+		human_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+		time_buffer.append(human_time)		
+
             	data_set = data.split(',')
 
             	tmp_volts = data_set[0]
@@ -54,7 +59,6 @@ while True:
             	windSpeed_MPH = data_set[4]
 
 		tmp_volts_buffer.append(tmp_volts)
-		print(str(tmp_volts)+"-->"+tmp_volts_buffer[data_buffer_count] +" added to tmp_volts_buffer. data_buffer_count= "+str(data_buffer_count))
 		tmp_celsius_buffer.append(tmp_celsius)
 		rv_volts_buffer.append(rv_volts)
 		zeroWind_volts_buffer.append(zeroWind_volts)
@@ -62,22 +66,24 @@ while True:
 		
 		data_buffer_count = data_buffer_count +1
 
-	    print("After while test")
-	    print(str(tmp_volts_buffer[0]))
-	    print(str(tmp_volts_buffer[1]))
-	    
 		
-	    print("Data smoothing with data_corr: "+str(data_corr)+" and tmp_volts_buffer: "+tmp_volts_buffer[0]+" "+tmp_volts_buffer[1]+" -->"+ str(data_smoothing(tmp_volts_buffer, data_corr)))	
+#	    print("Data smoothing with data_corr: "+str(data_corr)+" and tmp_volts_buffer: "+tmp_volts_buffer[0]+" "+tmp_volts_buffer[1]+" -->"+ str(data_smoothing(tmp_volts_buffer, data_corr)))	
 
 	    temp_tmp_volts_buffer = tmp_volts_buffer		
 	    if data_smoothing(temp_tmp_volts_buffer, data_corr) == 1:
-		print("Abweichung OK")
-		print("tmp_volts_buffer at index 0 "+str(tmp_volts_buffer[0]))
-		print("tmp_volts_buffer at index 1 "+str(tmp_volts_buffer[1]))
-	   
-#	    if(abs(100/data_buffer[0]*data_buffer[1]-100)<=data_corr):
-		 		
-	    print("######################################################")	
+		print("Fluctuation of tmp thermistor OK: "+ str(abs(100/float(tmp_volts_buffer[0])*float(tmp_volts_buffer[1])-100))+"%")
+		print("tmp thermistor value in volts of measurement n "+str(time_buffer[0])+" "+str(tmp_volts_buffer[0]))
+		print("tmp thermistor value in volts of measurement n+1 "+str(time_buffer[1])+" "+str(tmp_volts_buffer[1]))
+	 
+		sensor_node_data.write(str(time_buffer[0])+", "+str(tmp_volts_buffer[0])+"\n")
+		sensor_node_data.write(str(time_buffer[1])+", "+str(tmp_volts_buffer[1])+"\n")
+		
+		 
+		sensor_node_log.write("Fluctuation of tmp thermistor OK: "+ str(abs(100/float(tmp_volts_buffer[0])*float(tmp_volts_buffer[1])-100))+"%\n") 
+		sensor_node_log.write("tmp thermistor value in volts of measurement n "+str(time_buffer[0])+" "+str(tmp_volts_buffer[0])+"\n") 
+		sensor_node_log.write("tmp thermistor value in volts of measurement n+1 "+str(time_buffer[1])+" "+str(tmp_volts_buffer[1])+"\n") 
+	    else:
+		 print("Fluctuation of tmp thermistor NOT OK: "+ str(abs(100/float(tmp_volts_buffer[0])*float(tmp_volts_buffer[1])-100))+"%")	 		
 
 
 	    human_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
